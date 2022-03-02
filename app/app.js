3ecc7efa3b701ada8c8aa6ff8053e2ce1a1624b3
franco9535 <87659308+franco9535@users.noreply.github.com>
2022-03-02 10:25:42 +0300

    :art: update.ahmed_abdi.js

---
+++ app/app.js
@@ -0,0 +1,31 @@
+function ManchesterGoal(){
+ value =document.getElementById("goal1").innerHTML;
+ value++;
+ document.getElementById("goal1").innerHTML=value;
+}
+
+
+function liverpoolGoal(){
+    value = document.getElementById("goal2").innerHTML;
+    value++;
+    document.getElementById("goal2").innerHTML=value;
+    
+
+}
+
+function manchesterFoul(){
+    value = document.getElementById("foul1").innerHTML;
+    value++;
+    document.getElementById("foul1").innerHTML=value;
+}
+function liverpoolFoul (){
+    value =document.getElementById("foul2").innerHTML;
+    value++;
+    document.getElementById("foul2").innerHTML=value;
+
+}
+function resetGoal(){
+    value =document.getElementById("reset").innerHTML;
+    value++;
+    document.getElementById("reset").innerHTML=value;
+}
